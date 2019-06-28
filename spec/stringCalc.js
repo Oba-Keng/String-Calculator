1815340c5d1c7d9aefc2716e5d92c6449b07d1bd
Obakeng <obeezus@gmail.com>
2019-06-28 14:15:00 +0200

    second commit

--- spec/stringCalc.js
+++ spec/stringCalc.js
@@ -1,58 +1,29 @@
-function add(string){
+function add(string) {
+  var string;
+  //returns zero if no string is entered
+  if (string == "") {
+    return 0;
+  }
+  //regular expression for delimeters
+  let regex = /\d{1,5}/gm;
 
-    var string;
+  let number = string.match(regex);
 
-    if(string==""){
-
-            return 0;
-        
-        }
-
-    let regex = /\d{1,5}/gm;
-
-   
-    let number = string.match(regex);
-   
-   
-    let sum = 0;
-    // console.log(number)
-
-    if(string.includes('-')) throw new Error('negatives not allowed');
-
-    number.map((element,index)=>{
-      if(element>=1001){
-     
-
-        number.splice(index,1)
-      }
-    })
-
-
-    
-    
-
-    
-
-     for(let i=0;i<number.length;i++)
-     {
-     
-       
-      
-    
-
-      sum += parseInt(number[i]);
-     
-     }
-    return sum;
-    //  console.log(sum);
-
-    
-
-    
-    }     
-
-
+  let sum = 0;
+  //condition for negative numbers added
+  if (string.includes("-")) throw new Error("negatives not allowed");
+  //numbers greater than 1000 are ignored
+  number.map((element, index) => {
+    if (element >= 1001) {
+      number.splice(index, 1);
+    }
+  });
+  //returns the sum of the numbers added in a string
+  for (let i = 0; i < number.length; i++) {
+    sum += parseInt(number[i]);
+  }
+  return sum;
+}
 
 // add();
-module.exports = {add:add}
-
+module.exports = { add: add };
